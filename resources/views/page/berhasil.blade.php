5fb4a4248e3cc5cbb4140c75d5e1ac19cfbada5a
Uhdahlah <yogap5499@gmail.com>
2023-04-04 18:57:46 +0700

    Update brand

--- resources/views/page/berhasil.blade.php
+++ resources/views/page/berhasil.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark navbar-custom">
-    <div class="container"><a class="navbar-brand" href="#">7TOGK-ID</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand" href="#">7TOGKSMP</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="nav navbar-nav ml-auto">
             </ul><span class="text-white">{{ Auth::user()->Gamertag; }}</span><span class="navbar-text"></span><a href="{{route('logout')}}"></a><a class="btn btn-primary ml-auto" href="{{route('logout')}}">Logout</a></div>
