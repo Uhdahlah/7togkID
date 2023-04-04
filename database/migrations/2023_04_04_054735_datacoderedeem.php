1f4b8737891f7ca0359b86e0612c29a9d26da1fb
Uhdahlah <yogap5499@gmail.com>
2023-04-04 21:38:39 +0700

    change timezone

--- database/migrations/2023_04_04_054735_datacoderedeem.php
+++ database/migrations/2023_04_04_054735_datacoderedeem.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::create('datacoderedeem', function (Blueprint $table) {
-            $table->id();
             $table->string('Gamertag');
             $table->string('Checklist');
             $table->timestamps();
