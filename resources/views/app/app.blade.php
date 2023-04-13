4258d8b17248b1ea809ec5a43a933a4cb6f65049
Uhdahlah <yogap5499@gmail.com>
2023-04-13 15:36:05 +0700

    Update app.blade.php

--- resources/views/app/app.blade.php
+++ resources/views/app/app.blade.php
@@ -5,18 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>7TOGK-ID</title>
-    <link rel="shortcut icon" href="{{asset('assets')}}/img/icon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{secure_asset('assets')}}/img/icon.ico" type="image/x-icon" />
     <meta property="og:type" content="website">
     <meta name="description" content="game server indonesia">
     <meta name="author" content="Setya 7Togk">
-    <link rel="stylesheet" href="{{asset('assets')}}/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
-    <link rel="stylesheet" href="{{asset('assets')}}/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/Footer-Dark.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/Header-Dark.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/styles.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/css/Footer-Dark.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/css/Header-Dark.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/css/styles.css">
+    <link rel="stylesheet" href="{{secure_asset('assets')}}/css/Login-Form-Dark.css">
 </head>
 
 <body>
