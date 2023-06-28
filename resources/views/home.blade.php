449b9b066d4ac2d9e14fb7f2b0b223034306c2c4
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2023-06-28 15:32:33 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="masthead-content">
             <div class="container">
                 <h1 class="masthead-heading mb-0">7TOGK_ID</h1>
-                <h2 class="masthead-subheading mb-0">SERVER GAME INDONESIA</h2><a class="btn btn-success btn-xl rounded-pill mt-5" role="button" href="minecraft://?addExternalServer=7TogkSMP%20Revolution|play.7togkmc.id:19132">PLAY NOW</a></div>
+                <h2 class="masthead-subheading mb-0">SERVER GAME INDONESIA</h2><a class="btn btn-success btn-xl rounded-pill mt-5" role="button" href="minecraft://?addExternalServer=7TogkSMP%20Nightmare|play.7togkmc.id:19132">PLAY NOW</a></div>
         </div>
     </header>
     <section>
@@ -18,7 +18,7 @@
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
                         <h4 class="text-center display-4" style="font-size: 25px;">MINECRAFT'SERVER</h4>
-                        <p>Ayo Gabung Di Server 7TogkSMP Revolution Indonesia. Di server Ini Mempunyai Fitur Lumayan Banyak Loh. Apa Lagi yang Di tunggu Tunggu Yuk Gabung Di Server 7TogkSMP Revolution Indonesia.
+                        <p>Ayo Gabung Di Server 7TogkSMP Nightmare Indonesia. Di server Ini Mempunyai Fitur Lumayan Banyak Loh. Apa Lagi yang Di tunggu Tunggu Yuk Gabung Di Server 7TogkSMP Nightmare Indonesia.
 </p><p class="text-center"><?php $status = json_decode(file_get_contents('https://api.mcsrvstat.us/bedrock/2/play.7togkmc.id')); if($status->online == true){ echo "Online : 🧞‍♂️ ".$status->players->online." Players"; }else{ echo "status : offline";} ?></br>IP : play.7togkmc.id</br>
                            port : 19132</br>
                         version : 1.19.71</br>
